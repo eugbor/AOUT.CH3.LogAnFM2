86b1db5b23b79da6c9cf4ae515161281278f61bd
eugeneborodin <eugeneborodin@mail.ru>
2017-02-06 01:02:07 +0300

    init

---
+++ AOUT.CH3.LogAnFM2.UnitTests/LogAnalyzerUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AOUT.CH3.LogAnFM2.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void overrideTestWithoutStub()
+        {
+            TestableLogAnalyzer logan = new TestableLogAnalyzer();
+            logan.IsSupported = true;
+            bool result = logan.IsValidLogFileName("file.exe");
+            Assert.True(result,"...");
+        }
+        class TestableLogAnalyzer : LogAnalyzerUsingFactoryMethod
+        {
+            public bool IsSupported;
+            protected override bool IsValid(string fileName)
+            {
+                return IsSupported;
+            }
+        }
+    }
+}
---
+++ AOUT.CH3.LogAnFM2/LogAnalyzerUsingFactoryMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOUT.CH3.LogAnFM2
+{
+    public interface IExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+    public class FileExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return true;
+            // здесь читается файл                   
+        }
+    }
+
+    public class LogAnalyzerUsingFactoryMethod
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return this.IsValid(fileName);
+        }
+
+        protected virtual bool IsValid(string fileName)
+        {
+            FileExtensionManager mgr = new FileExtensionManager();
+            return mgr.IsValid(fileName);
+        }
+    }
+}
